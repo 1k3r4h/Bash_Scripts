639023bb9e3c6f266b2a3cd081b6fe409eba761a
Iker <ikerkatache@protonmail.com>
2026-02-20 15:02:04 +0100

    Delete tor.sh

--- tor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if  ! command -v torbrowser-launcher &> /dev/null
-then
-	echo "Tor is  not installer in this device if you wanna continue pleas install it"
-	exit 1
-fi
-
-torbrowser-launcher
